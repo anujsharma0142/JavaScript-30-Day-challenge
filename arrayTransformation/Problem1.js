479fea45d8c3288f30d1ec24f180fc45bb29eacf
anujsharma0142 <anujsharma0142@mail.com>
2024-11-27 19:23:54 +0530

    added arrayProblem without map function

---
+++ arrayTransformation/Problem1.js
@@ -0,0 +1,12 @@
+// Leetcode 2635
+//without map function
+
+
+
+var map = function(arr, fn) {
+    const newArr = [];
+    for(let i=0; i<arr.length; i++){
+       newArr[i] = fn(arr[i],i)
+    }
+    return newArr;
+};
